d23af5c8ddfcb26f30f6b0658a609835ffb155e0
Gbolu <gboluwagaadeyemi@gmail.com>
2020-12-22 13:18:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lenovolaptopsproj/HomePage.java
@@ -0,0 +1,17 @@
+package com.example.lenovolaptopsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+    }
+}
---
+++ app/src/main/java/com/example/lenovolaptopsproj/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.lenovolaptopsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),HomePage.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+    }
+}
